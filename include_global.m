bf43a65cc7a54a25f1cdb6d733aad94777e6d7ab
Arnauld Sergé <arnauld.serge@inserm.fr>
2022-06-27 10:34:02 +0200

    Add files via upload

---
+++ include_global.m
@@ -0,0 +1,33 @@
+global Nb_STK ;
+global T ;
+global T_off ;
+global sig_free ;
+global Boule_free ;
+global Nb_combi ;
+global Poids_melange_aplha ;
+global Poids_melange_diff ;
+global VERSION ;
+
+global OPTIM_R 
+
+global N_PARAM PARAM_T PARAM_I PARAM_J PARAM_ALPHA PARAM_RADIUS_I ...
+    PARAM_OFFSET PARAM_BLINK PARAM_SIG2 PARAM_K PARAM_RADIUS_J
+global SAVED_PARAMS SAVE_VAR_MOY
+
+global im_t ;
+global tab_param ;
+global tab_var ;
+global tab_moy ;
+
+global stderr
+
+global USE_MAT
+
+global tab_num
+
+global CROP IRANGE JRANGE
+global FORMAT_IM
+
+global AFFICHAGE SHOW;
+global MTT_version ;
+%%%
